4fcd9bede3cb64f8ad248b1ea79b098e8b30c695
DarioCespedes <111027360+DarioCespedes@users.noreply.github.com>
2024-07-27 10:44:36 -0500

    Add files via upload
    Lesson 9

---
+++ SQL_Easy/Sql_Easy.sql
@@ -0,0 +1,25 @@
+--Ejercicios tomados de  :  https://www.sql-easy.com/ 
+
+-- SELECT *
+SELECT * FROM family_members
+
+--SELECT specific columns
+SELECT name, species FROM family_members
+
+--WHERE ... Equals
+SELECT * FROM family_members WHERE species = "dog"
+
+--WHERE ... Greater than
+SELECT * FROM family_members WHERE num_books_read > 190
+
+-- WHERE ... Greater than or equal
+SELECT * FROM family_members WHERE num_books_read >= 180
+
+--AND
+SELECT * FROM friends_of_pickles WHERE height_cm <45 AND species = 'dog'
+
+--OR
+SELECT * FROM friends_of_pickles WHERE height_cm < 45 OR species = 'dog';
+
+--IN
+SELECT * FROM friends_of_pickles WHERE species NOT IN ('cat', 'dog');
